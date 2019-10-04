68385991b022670e40ad5532c1385f98b151f115
Stoyan <slownessbg@gmail.com>
2019-10-04 23:54:54 +0300

    adding Config Manager

---
+++ ChampionStatistics/ConfigManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ChampionStatistics
+{
+    public class ConfigManager<T>
+    {
+        public T ConfigClass { get; }
+
+        public ConfigManager(string path)
+        {
+            this.ConfigClass = JsonConvert.DeserializeObject<T>(File.ReadAllText(path));
+        }
+    }
+}
